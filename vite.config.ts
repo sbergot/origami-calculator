452ba286f96bc995960eda5eefee39c7b8ee1775
Simon Bergot <sbergot@talentsoft.com>
2022-01-01 11:56:23 +0100

    add masu to production build

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
+const { resolve } = require('path')
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        nested: resolve(__dirname, 'masu/index.html')
+      }
+    }
+  }
+});
