c7cf4262319c3ed231e87dd91d416b8c9a9f86a0
Simon Bergot <sbergot@talentsoft.com>
2022-01-02 18:55:19 +0100

    improve masu page

--- root/src/Masu/App.tsx
+++ root/src/Masu/App.tsx
@@ -5,10 +5,13 @@ import { MathJaxContainer, YoutubeEmbed } from "../Layout";
 export default function App() {
   return (
     <MathJaxContainer>
-      <div className="mt-4">
+      <div className="fluid-container mt-4">
         <MasuBox />
+        <YoutubeEmbed embedId="V2q5CyjfEKs" className="max-w-md" />
         <Divisor1 />
+        <YoutubeEmbed embedId="8r0MMfT0b2I" className="max-w-md" />
         <Divisor2 />
+        <YoutubeEmbed embedId="TkEIyBQ1xic" className="max-w-md" />
       </div>
     </MathJaxContainer>
   );
@@ -22,35 +25,30 @@ function MasuBox() {
   const [boxWidth, setWidth] = useState(49);
   const sheetWidth = (boxWidth / Math.sqrt(2)) * 4;
   return (
-    <>
+    <div>
       <h2 className="text-lg font-bold">Boîte Masu</h2>
-      <div className="fluid-container">
-        <div>
-          <div>
-            <p>Largeur de la boîte</p>
-            <input
-              type="number"
-              className="border px-2"
-              value={boxWidth}
-              onChange={(e) => setWidth(parseInt(e.currentTarget.value))}
-            />
-          </div>
-          <div className="mt-4 grid grid-cols-2fc">
-            <MathJax style={{ display: "inline-block" }}>
-              {
-                "\\(largeur\\_feuille = \\frac{largeur\\_boîte \\times 4}{\\sqrt{2}}\\)"
-              }
-            </MathJax>
-            <span className="ml-2">=&nbsp;{formatResult(sheetWidth)}</span>
-            <MathJax style={{ display: "inline-block" }} className="mt-2">
-              {"\\(hauteur\\_boîte = \\frac{largeur\\_boîte}{2}\\)"}
-            </MathJax>
-            <span className="ml-2">=&nbsp;{formatResult(boxWidth / 2)}</span>
-          </div>
-        </div>
-        <YoutubeEmbed embedId="V2q5CyjfEKs" className="max-w-md" />
+      <div>
+        <p>Largeur de la boîte</p>
+        <input
+          type="number"
+          className="border px-2"
+          value={boxWidth}
+          onChange={(e) => setWidth(parseInt(e.currentTarget.value))}
+        />
       </div>
-    </>
+      <div className="mt-4 grid grid-cols-2fc">
+        <MathJax style={{ display: "inline-block" }}>
+          {
+            "\\(largeur\\_feuille = \\frac{largeur\\_boîte \\times 4}{\\sqrt{2}}\\)"
+          }
+        </MathJax>
+        <span className="ml-2">=&nbsp;{formatResult(sheetWidth)}</span>
+        <MathJax style={{ display: "inline-block" }} className="mt-2">
+          {"\\(hauteur\\_boîte = \\frac{largeur\\_boîte}{2}\\)"}
+        </MathJax>
+        <span className="ml-2">=&nbsp;{formatResult(boxWidth / 2)}</span>
+      </div>
+    </div>
   );
 }
 
@@ -58,27 +56,22 @@ function Divisor1() {
   const [boxWidth, setWidth] = useState(4);
   const sheetWidth = boxWidth * 3;
   return (
-    <div className="mt-4">
+    <div>
       <h2 className="text-lg font-bold">Diviseur en +</h2>
-      <div className="fluid-container">
-        <div>
-          <div>
-            <p>Largeur du diviseur</p>
-            <input
-              type="number"
-              className="border px-2"
-              value={boxWidth}
-              onChange={(e) => setWidth(parseInt(e.currentTarget.value))}
-            />
-          </div>
-          <div className="mt-4 grid grid-cols-2fc">
-            <MathJax style={{ display: "inline-block" }}>
-              {"\\(largeur\\_feuille = largeur\\_diviseur \\times 3\\)"}
-            </MathJax>
-            <span className="ml-2">=&nbsp;{formatResult(sheetWidth)}</span>
-          </div>
-        </div>
-        <YoutubeEmbed embedId="8r0MMfT0b2I" className="max-w-md" />
+      <div>
+        <p>Largeur du diviseur</p>
+        <input
+          type="number"
+          className="border px-2"
+          value={boxWidth}
+          onChange={(e) => setWidth(parseInt(e.currentTarget.value))}
+        />
+      </div>
+      <div className="mt-4 grid grid-cols-2fc">
+        <MathJax style={{ display: "inline-block" }}>
+          {"\\(largeur\\_feuille = largeur\\_diviseur \\times 3\\)"}
+        </MathJax>
+        <span className="ml-2">=&nbsp;{formatResult(sheetWidth)}</span>
       </div>
     </div>
   );
@@ -88,29 +81,24 @@ function Divisor2() {
   const [boxWidth, setWidth] = useState(4);
   const sheetWidth = boxWidth * (4 / Math.sqrt(2));
   return (
-    <div className="mt-4">
+    <div>
       <h2 className="text-lg font-bold">Diviseur en X</h2>
-      <div className="fluid-container">
-        <div>
-          <div>
-            <p>Largeur du diviseur</p>
-            <input
-              type="number"
-              className="border px-2"
-              value={boxWidth}
-              onChange={(e) => setWidth(parseInt(e.currentTarget.value))}
-            />
-          </div>
-          <div className="mt-4 grid grid-cols-2fc">
-            <MathJax style={{ display: "inline-block" }}>
-              {
-                "\\(largeur\\_feuille = \\frac{largeur\\_boîte \\times 4}{\\sqrt{2}}\\)"
-              }
-            </MathJax>
-            <span className="ml-2">=&nbsp;{formatResult(sheetWidth)}</span>
-          </div>
-        </div>
-        <YoutubeEmbed embedId="TkEIyBQ1xic" className="max-w-md" />
+      <div>
+        <p>Largeur du diviseur</p>
+        <input
+          type="number"
+          className="border px-2"
+          value={boxWidth}
+          onChange={(e) => setWidth(parseInt(e.currentTarget.value))}
+        />
+      </div>
+      <div className="mt-4 grid grid-cols-2fc">
+        <MathJax style={{ display: "inline-block" }}>
+          {
+            "\\(largeur\\_feuille = \\frac{largeur\\_boîte \\times 4}{\\sqrt{2}}\\)"
+          }
+        </MathJax>
+        <span className="ml-2">=&nbsp;{formatResult(sheetWidth)}</span>
       </div>
     </div>
   );
