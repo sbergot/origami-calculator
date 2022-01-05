86bacbe4c3e2e65273ae3fb6cd35282b65c75de1
Simon Bergot <sbergot@talentsoft.com>
2022-01-05 17:16:37 +0100

    improve cache policy

--- root/public/worker.js
+++ root/public/worker.js
@@ -1,57 +1,96 @@
-'use strict';
+"use strict";
 
-const version = 'v1::';
+const version = "v1::";
 
-const cachename = version + 'pages';
+const cachename = version + "pages";
 
-self.addEventListener('fetch', function (event) {
-    if (event.request.method !== 'GET') {
-        return;
-    }
-    event.respondWith(
-        caches.match(event.request).then(function (cached) {
-            var networked = fetch(event.request)
-                .then(fetchedFromNetwork, unableToResolve)
-                .catch(unableToResolve);
+function getErrorResponse() {
+  return new Response("<h1>Service Unavailable</h1>", {
+    status: 503,
+    statusText: "Service Unavailable",
+    headers: new Headers({
+      "Content-Type": "text/html",
+    }),
+  });
+}
 
-            return cached || networked;
+function cacheFirstResponse(event) {
+  event.respondWith(
+    caches.match(event.request).then(function (cached) {
+      var networked = fetch(event.request)
+        .then(fetchedFromNetwork, unableToResolve)
+        .catch(unableToResolve);
 
-            function fetchedFromNetwork(response) {
-                var cacheCopy = response.clone();
+      return cached || networked;
 
-                caches.open(version + 'pages').then(function add(cache) {
-                    console.log("updating " + event.request.url);
-                    return cache.put(event.request, cacheCopy);
-                });
+      function fetchedFromNetwork(response) {
+        var cacheCopy = response.clone();
 
-                return response;
-            }
+        caches.open(version + "pages").then(function add(cache) {
+          console.log("updating " + event.request.url);
+          return cache.put(event.request, cacheCopy);
+        });
 
-            function unableToResolve() {
-                return new Response('<h1>Service Unavailable</h1>', {
-                    status: 503,
-                    statusText: 'Service Unavailable',
-                    headers: new Headers({
-                        'Content-Type': 'text/html',
-                    }),
-                });
-            }
-        }),
-    );
+        return response;
+      }
+
+      function unableToResolve() {
+        return getErrorResponse();
+      }
+    })
+  );
+}
+
+function networkFirstResponse(event) {
+  event.respondWith(
+    caches.match(event.request).then(async function (cached) {
+      var networked = await fetch(event.request)
+        .then(fetchedFromNetwork, unableToResolve)
+        .catch(unableToResolve);
+
+      return networked || cached || getErrorResponse();
+
+      function fetchedFromNetwork(response) {
+        var cacheCopy = response.clone();
+
+        caches.open(version + "pages").then(function add(cache) {
+          console.log("updating " + event.request.url);
+          return cache.put(event.request, cacheCopy);
+        });
+
+        return response;
+      }
+
+      function unableToResolve() {
+        return null;
+      }
+    })
+  );
+}
+
+self.addEventListener("fetch", function (event) {
+  if (event.request.method !== "GET") {
+    return;
+  }
+  if (event.request.destination === "document") {
+    networkFirstResponse(event);
+  } else {
+    cacheFirstResponse(event);
+  }
 });
 
-self.addEventListener('activate', function (event) {
-    event.waitUntil(
-        caches.keys().then(function (keys) {
-            return Promise.all(
-                keys
-                    .filter(function (key) {
-                        return !key.startsWith(version);
-                    })
-                    .map(function (key) {
-                        return caches.delete(key);
-                    }),
-            );
-        }),
-    );
+self.addEventListener("activate", function (event) {
+  event.waitUntil(
+    caches.keys().then(function (keys) {
+      return Promise.all(
+        keys
+          .filter(function (key) {
+            return !key.startsWith(version);
+          })
+          .map(function (key) {
+            return caches.delete(key);
+          })
+      );
+    })
+  );
 });
